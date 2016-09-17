48fda469626f408fd2621fefaf69ba9c9ae5b3ca
linan <linan@f-road.com.cn>
2016-09-17 14:26:10 +0800

    ready init

---
+++ ready-dynamic-webext/src/main/java/org/ready/dynamic/webext/App.java
@@ -0,0 +1,13 @@
+package org.ready.dynamic.webext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
